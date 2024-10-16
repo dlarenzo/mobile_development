b7d929b2dab8bab1be53d2eeb093551c53cac6b2
LaRenzo <dlarenzo@outlook.com>
2024-10-16 14:13:50 -0600

    completed Goal App

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, View, FlatList, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+// IMPORT CHILDREN COMPONENTS
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  // call useState and set the State for enteredGoal (THIS WILL BE MOVED TO GoalInput.js SO COMMENT OUT HERE)
+  // const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  // manage list of goals with useState.  Remember it will initialize with an empty array like input does with an empty string
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  // UseState for Modal
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  // Modal Handler
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
+
+  // Add function for goal input handler.  (THIS WILL BE MOVED TO GoalInput.js SO COMMENT OUT HERE)
+  // function goalInputHandler(enteredText) {
+  //   setEnteredGoalText(enteredText);
+  // }
+
+  // Add function for add goal handler
+  function addGoalHandler(enteredGoalText) {
+    // call our setCourseGoals function and update our courseGoals array with the new goal
+    // one way we could use the spread operator to add the new goal to the existing array
+    // setCourseGoals([...courseGoals, enteredGoalText]);
+
+    // BETTER WAY TO DO THIS BELOW(best practice):
+    // But if your useState is dependent on the previous state, you should use the function form of setState
+    // this function will automatically be called by React and will pass the previous state as an argument
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+    endAddGoalHandler();
+  }
+
+  function deleteGoalHandler(id) {
+    // call our setCourseGoals function and update our new state
+    setCourseGoals((currentCourseGoals) => {
+      // filter out the goal that has the id we want to delete
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        {/* Button is a built-in component that we are passing the title prop to
+        and the onPress prop to */}
+        <Button
+          title="Add New Goal"
+          color="#a065ec"
+          onPress={startAddGoalHandler}
+        />
+        {/* GoalInput is a child component that we are passing the onAddGoal prop to */}
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style={styles.goalsContainer}>
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              // GoalItem is a child component that we are passing the text prop to
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingHorizontal: 16,
+  },
+
+  goalsContainer: {
+    flex: 5,
   },
 });
---
+++ components/GoalInput.js
@@ -0,0 +1,84 @@
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from "react-native";
+import { useState } from "react";
+
+function GoalInput(props) {
+  // call useState and set the State for enteredGoal (MOVED HERE FROM App.js)
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  // Add function for goal input handler.  (MOVED FROM App.js HERE)
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" color="#f31282" onPress={props.onCancel} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+export default GoalInput;
+
+// SET UP STYLESHEET THAT BELONGS TO THIS COMPONENT
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    backgroundColor: "#311b6b",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    borderRadius: 6,
+    color: "#120438",
+    width: "100%",
+    padding: 16,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
+});
